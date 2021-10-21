f29b38674c37559131f9e19a4868ae4887aac228
makxemel <delmst@yandex.ru>
2021-10-21 20:56:18 +0300

    header layout

---
+++ components/Header/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Paper, Button, IconButton, Avatar } from '@material-ui/core';
+import {
+  SearchOutlined as SearchIcon,
+  Create as PenIcon,
+  SmsOutlined as MessageIcon,
+  NotificationsNoneOutlined as NotificationIcon,
+  Menu as MenuIcon,
+} from '@material-ui/icons/';
+
+import styles from './Header.module.scss';
+
+export const Header: React.FC = () => {
+  return (
+    <Paper classes={{ root: styles.root }} elevation={0}>
+      <div className="d-flex align-center">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <svg className={styles.logo} viewBox="0 0 24 25" id="site-logo">
+          <path fill="#e8a427" d="M0 19h8.5v6H0v-6z"></path>
+          <path d="M0 7h8.5v18l6.5-6V7h9V0H0v7z"></path>
+          <path fill="rgba(0,0,0,0.15)" d="M7.5 19h1v6l-1-6z"></path>
+        </svg>
+
+        <div className={styles.searchBlock}>
+          <SearchIcon />
+          <input placeholder="Поиск" />
+        </div>
+
+        <Button className={styles.penButton} variant="contained">
+          <PenIcon />
+        </Button>
+      </div>
+      <div className="d-flex align-center">
+        <IconButton>
+          <MessageIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationIcon />
+        </IconButton>
+        <Avatar className="ml-15" alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+      </div>
+    </Paper>
+  );
+};
---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from 'next/app';
+import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
+import { theme } from '../theme';
+
+import '../styles/globals.css';
+import 'macro-css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />;
+    </MuiThemeProvider>
+  );
+}
+export default MyApp;
