b94c5239f5e31fb4005edbe53d9daaf2a4d9b844
makxemel <delmst@yandex.ru>
2021-10-25 00:59:12 +0300

    LeftMenu component

---
+++ components/LeftMenu/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import { Whatshot as FireIcon, SmsOutlined as MessageIcon, TrendingUp as RateIcon, FormatListBulleted as ListIcon} from '@material-ui/icons/';
+
+import styles from './LeftMenu.module.scss';
+
+export const LeftMenu: React.FC = () => {
+  return (
+    <div className={styles.menu}>
+      <ul>
+        <li><Button><FireIcon />Лента</Button></li>
+        <li><Button><MessageIcon />Сообщения</Button></li>
+        <li><Button><RateIcon />Рейтинг TJ</Button></li>
+        <li><Button><ListIcon />Подписки</Button></li>
+      </ul>
+    </div>
+  );
+};
