e54310b6df6b210ab270a545ff6d5d6bba7d1f3e
makxemel <delmst@yandex.ru>
2021-11-01 01:12:54 +0300

    added ui to LeftMenu

--- components/LeftMenu/index.tsx
+++ components/LeftMenu/index.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
+import Link from 'next/link';
 import { Button } from '@material-ui/core';
-import { Whatshot as FireIcon, SmsOutlined as MessageIcon, TrendingUp as RateIcon, FormatListBulleted as ListIcon} from '@material-ui/icons/';
+import {
+  WhatshotOutlined as FireIcon,
+  SmsOutlined as MessageIcon,
+  TrendingUpOutlined as TrendingIcon,
+  FormatListBulletedOutlined as ListIcon,
+} from '@material-ui/icons';
 
 import styles from './LeftMenu.module.scss';
+
+const menu = [
+  { text: 'Лента', icon: <FireIcon />, path: '/' },
+  { text: 'Сообщения', icon: <MessageIcon />, path: '/messages' },
+  { text: 'Рейтинг RJ', icon: <TrendingIcon />, path: '/rating' },
+  { text: 'Подписки', icon: <ListIcon />, path: '/follows' },
+];
 
 export const LeftMenu: React.FC = () => {
   return (
     <div className={styles.menu}>
       <ul>
-        <li><Button><FireIcon />Лента</Button></li>
-        <li><Button><MessageIcon />Сообщения</Button></li>
-        <li><Button><RateIcon />Рейтинг TJ</Button></li>
-        <li><Button><ListIcon />Подписки</Button></li>
+        {menu.map((obj) => (
+          <li key={obj.path}>
+            <Link href={obj.path}>
+              <Button>
+                {obj.icon}
+                {obj.text}
+              </Button>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
